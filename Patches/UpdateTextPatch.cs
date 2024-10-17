32c79760b0775011083054e6782187fd165f2366
Altertoriel <op58274@gmail.com>
2024-10-17 19:44:05 +0900

    UpdateText Patches

---
+++ Patches/UpdateTextPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using UnityEngine;
+namespace yandEX.Patches
+{
+    [HarmonyPatch(typeof(UpdateCheckScript))]
+    [HarmonyPatch("Start")]
+    internal class UpdateTextPatch
+    {
+        static void Postfix(UpdateCheckScript __instance)
+        {
+            __instance.warningLabel.text = "This is Modded version of Yandere Simulator!";
+            __instance.warningLabel.color = Color.green;
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,28 +1,31 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using System;
 using yandEX.Patches;
 
 namespace YandEX {
-    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInPlugin(modGUID, "YanderEX", verNum)]
     public class ModBase : BaseUnityPlugin {
-        const string modGUID = "Altertoriel.YandEX";
-        const string modName = "YanderEX";
-        const string modVersion = "1.0.0.0";
+        const string modGUID = "altertoriel.yandex";
+        const string verNum = "1.0.0.0";
 
-        readonly Harmony harmony = new Harmony(modGUID);
+        public static Version Version => new Version(verNum);
 
         internal static ModBase i;
         internal ManualLogSource mls;
 
         void Awake() {
-            if (i == null) i = this;
+            i = this;
+            Harmony harmony = new Harmony(modGUID);
 
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
             mls.LogInfo("Mod Installed Successfully.");
+            mls.LogInfo($"Mod Version: {verNum}");
 
             harmony.PatchAll(typeof(ModBase));
             harmony.PatchAll(typeof(AntiCheatBypass));
+            harmony.PatchAll(typeof(UpdateTextPatch));
         }
     }
 }
